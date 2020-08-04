243cf60abdcc21e36b29ecaba4bcf95c7641852d
Durga Prasad Sahoo <dpshoo.cs@gmail.com>
2020-08-03 22:48:39 -0700

    strace-tutorial

---
+++ strace/add_two_ints.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+
+int main(){
+
+    int n=10;
+    int sum = 0;
+    int i;
+
+    for(i=0; i<10; i++){
+        sum += i;
+    }
+    printf("Sum of numbers less than 10: %d", sum);
+
+}
+
